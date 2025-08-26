c30222a3845bab0b8b852ae23e0cec90b9b00687
shubham-mishra-2003 <shubhammishra3070@gmail.com>
2025-08-26 15:27:10 +0530

    card hover effect

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -10,7 +10,7 @@ const Card = React.forwardRef<
   <div
     ref={ref}
     className={cn(
-      "rounded-lg border bg-card text-card-foreground shadow-sm",
+      "rounded-2xl hover:-translate-y-2 duration-300 hover:shadow-2xl border bg-card text-card-foreground",
       className
     )}
     {...props}
